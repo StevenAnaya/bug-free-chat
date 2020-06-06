246df526a1323a5c9276b78052f78594d7e08ab7
Steven Anaya <stiven@munitienda.com>
2020-06-06 17:24:28 -0500

    Add basic router

---
+++ src/pages/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import { StyledChatMessage } from 'components/ChatMessage';
+
+const Router = () => {
+	return (
+		<Switch>
+			<Route path={['/isNotMine/:id', '/other']} strict exact>
+				<StyledChatMessage>
+					hola mundo
+				</StyledChatMessage>
+			</Route>
+			<Route path="/isMine" strict exact>
+				<StyledChatMessage isMine>
+					hola mundo AWADAWDAWDASDAW
+				</StyledChatMessage>
+			</Route>
+			<Route path="/" strict exact>
+				<p>Home</p>
+			</Route>
+			<Route>
+				<p>404 Not Found</p>
+			</Route>
+		</Switch >
+	)
+};
+
+export default Router;
